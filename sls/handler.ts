9be1d2c2cebecbb583286ac67269c45582bbe99c
Aaron Lin <aaronlin256@gmail.com>
2019-10-27 11:57:48 -0400

    Add handler for send phone code

--- sls/handler.ts
+++ sls/handler.ts
@@ -1,13 +1,31 @@
 import 'source-map-support/register';
 const axios = require('axios');
 
+export const sendPhoneCode = async (event, _context) => {
+  let axiosConfig = {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_5 like Mac OS X) AppleWebKit/604.5.6 (KHTML, like Gecko) Mobile/15D60 AKiOSSDK/4.29.0'
+    }
+  }
+
+  let codeRequestUrl = 'https://graph.accountkit.com/v1.2/start_login?access_token=AA%7C464891386855067%7Cd1891abb4b0bcdfa0580d9b839f4a522&credentials_type=phone_number&fb_app_events_enabled=1&fields=privacy_policy%2Cterms_of_service&locale=en_US&phone_number=#placeholder&response_type=token&sdk=ios'
+  codeRequestUrl = codeRequestUrl.replace(/#placeholder/, event)
+
+  axios.post(codeRequestUrl, '', axiosConfig)
+  .then(_res => {
+    console.log("RESPONSE RECEIVED", _res);
+  })
+  .catch(err => {
+    console.log("AXIOS ERROR: ", err);
+  })
+
+  // This may not work since we need the request code 
+  // which then leads to the confirmation code
+
+}
+
 export const setLocation = async (event, _context) => {
   console.log('event', event);
-
-  var postData = {
-      "lat": 34.6937,
-      "lon": 135.5023
-  };
   
   let axiosConfig = {
     headers: {
@@ -17,9 +35,9 @@ export const setLocation = async (event, _context) => {
     }
   };
   
-  axios.post('https://api.gotinder.com/user/ping', postData, axiosConfig)
+  axios.post('https://api.gotinder.com/user/ping', event, axiosConfig)
   .then(_res => {
-    console.log("RESPONSE RECEIVED");
+    console.log("RESPONSE RECEIVED", _res);
   })
   .catch(err => {
     console.log("AXIOS ERROR: ", err);
