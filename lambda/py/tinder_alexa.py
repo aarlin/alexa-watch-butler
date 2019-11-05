71ab27076a66f73119ffcf1ed7c9806d1ba84bc8
Aaron Lin <aaronlin256@gmail.com>
2019-11-04 22:47:40 -0500

    Got to phone stage asking for request code

--- lambda/py/tinder_alexa.py
+++ lambda/py/tinder_alexa.py
@@ -5,167 +5,245 @@
 import logging
 
 from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
+from phone_auth import send_phone_code, get_token_through_phone
+from tinder_api import set_location
+
 sb = SkillBuilder()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
-def launch_request_handler(handler_input):
+class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
-    # type: (HandlerInput) -> Response
-    speech_text = (
-        "Welcome to the Tinder Lite! "
-        "Would you like to authenicate with phone or authenticate through email?")
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("LaunchRequest")(handler_input)
 
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).set_should_end_session(
-        False).response
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = (
+            "Welcome to the Tinder Lite! "
+            "Would you like to authenicate with phone or authenticate through email?")
 
-@sb.request_handler(can_handle_func=is_request_type("PhoneAuthIntent"))
-def phone_auth_handler(handler_input):
-    """Handler for Skill Launch."""
-    # type: (HandlerInput) -> Response
-    speech_text = "To access Tinder, we will send a request code to you. What is your phone number?"
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False).response
 
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
-        False).response
+class PhoneAuthIntentHandler(AbstractRequestHandler):
+    """Handler for Phone Auth Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("PhoneAuthIntent")(handler_input)
+        
+    def handle(self, handler_input):
+        """Handler for Skill Launch."""
+        # type: (HandlerInput) -> Response
+        speech_text = "To access Tinder, we will send a request code to you. What is your phone number?"
 
-@sb.request_handler(can_handle_func=is_request_type("FacebookAuthIntent"))
-def facebook_auth_handler(handler_input):
-    """Handler for Skill Launch."""
-    # type: (HandlerInput) -> Response
-    speech_text = "To access Tinder, what is your Facebook email and password?"
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
+            False).response
 
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Facebook Authentication", speech_text)).set_should_end_session(
-        False).response
+class FacebookAuthIntentHandler(AbstractRequestHandler):
+    """Handler for Facebook Auth Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("FacebookAuthIntent")(handler_input)
 
-@sb.request_handler(can_handle_func=is_intent_name("PhoneRequestCodeIntent"))
-def phone_request_code_intent_handler(handler_input):
-    """Handler for Phone Authentication Intent."""
-    # type: (HandlerInput) -> Response
+    def handle(self, handler_input):
+        """Handler for Skill Launch."""
+        # type: (HandlerInput) -> Response
+        speech_text = "To access Tinder, what is your Facebook email and password?"
 
-    slots = handler_input.request_envelope.request.intent.slots
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Facebook Authentication", speech_text)).set_should_end_session(
+            False).response
 
-    if 'PhoneNumber' in slots:
-        phone_number = slots['PhoneNumber'].value
-        handler_input.attributes_manager.session_attributes['PHONE_NUMBER'] = phone_number
-    else:
-        speech = "I'm not sure what your phone number is, please try again"
-        reprompt = ("I'm not sure what your phone number is. "
-                    "You can tell me your phone number by saying, "
-                    "my phone number is ")
-
-    speech_text = "What is the request code that was sent to your phone?"
-
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
-        True).response
-
-@sb.request_handler(can_handle_func=is_intent_name("PhoneAuthenticationIntent"))
-def phone_authentication_intent_handler(handler_input):
-    """Handler for Phone Authentication Intent."""
-    # type: (HandlerInput) -> Response
-    slots = handler_input.request_envelope.request.intent.slots
-
-    if 'RequestCode' in slots:
-        request_code = slots['RequestCode'].value
-        handler_input.attributes_manager.session_attributes['REQUEST_CODE'] = request_code
-    else:
-        speech = "I'm not sure what your request code is, please try again"
-        reprompt = ("I'm not sure what your request code is. "
-                    "You can tell me your request code by saying, "
-                    "my request code is ")
-
-    speech_text = "Okay, we have you authenticated. Where do you want to set your location to?"
-
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
-        True).response
-
-# set location to <city>
-@sb.request_handler(can_handle_func=is_intent_name("SetLocationIntent"))
-def set_location_intent_handler(handler_input):
+class PhoneRequestCodeIntentHandler(AbstractRequestHandler):
     """Handler for Hello World Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "We set your location to this New York"
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("PhoneRequestCodeIntent")(handler_input)
 
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Location Set", speech_text)).set_should_end_session(
-        True).response
+    def handle(self, handler_input):
+        """Handler for Phone Authentication Intent."""
+        # type: (HandlerInput) -> Response
 
-@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
-def help_intent_handler(handler_input):
+        slots = handler_input.request_envelope.request.intent.slots
+
+        if 'PhoneNumber' in slots:
+            phone_number = slots['PhoneNumber'].value
+            handler_input.attributes_manager.session_attributes['PHONE_NUMBER'] = phone_number
+            confirmation_code = send_phone_code(phone_number)
+            handler_input.attributes_manager.session_attributes['CONFIRMATION_CODE'] = confirmation_code
+        else:
+            speech = "I'm not sure what your phone number is, please try again"
+            reprompt = ("I'm not sure what your phone number is. "
+                        "You can tell me your phone number by saying, "
+                        "my phone number is ")
+
+        speech_text = "What is the request code that was sent to your phone?"
+
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
+            True).response
+
+class PhoneAuthenticationIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("PhoneAuthenticationIntent")(handler_input)
+
+    def handle(self, handler_input):
+        """Handler for Phone Authentication Intent."""
+        # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        session_attributes = handler_input.attributes_manager.session_attributes
+
+        if 'ConfirmationCode' in slots:
+            confirmation_code = slots['ConfirmationCode'].value
+            handler_input.attributes_manager.session_attributes['CONFIRMATION_CODE'] = confirmation_code
+            auth_token = get_token_through_phone(confirmation_code, session_attributes['PHONE_NUMBER'], session_attributes['REQUEST_CODE'])
+
+            handler_input.attributes_manager.session_attributes['AUTH_TOKEN'] = auth_token
+        else:
+            speech = "I'm not sure what your request code is, please try again"
+            reprompt = ("I'm not sure what your request code is. "
+                        "You can tell me your request code by saying, "
+                        "my request code is ")
+
+        speech_text = "Okay, we have you authenticated. Where do you want to set your location to?"
+
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Phone Authentication", speech_text)).set_should_end_session(
+            True).response
+
+class SetLocationIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("SetLocationIntent")(handler_input)
+
+    def handle(self, handler_input):
+        """Handler for Hello World Intent."""
+        # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        session_attributes = handler_input.attributes_manager.session_attributes
+
+        if 'City' in slots:
+            city = slots['City'].value
+            session_attributes['CITY'] = city
+            response = set_location(session_attributes['AUTH_TOKEN'], city)
+        else:
+            speech = "I'm not sure what city you asked for, please try again"
+            reprompt = ("I'm not sure what city you set your location to. "
+                        "You can tell me your set city location by saying, "
+                        "set my location to ")
+
+        speech_text = "We set your location to {}".format(city)
+
+        return handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Location Set", speech_text)).set_should_end_session(
+            True).response
+
+class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "You can say hello to me!"
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
 
-    return handler_input.response_builder.speak(speech_text).ask(
-        speech_text).set_card(SimpleCard(
-            "Hello World", speech_text)).response
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "You can say hello to me!"
 
+        handler_input.response_builder.speak(speech_text).ask(
+            speech_text).set_card(SimpleCard(
+                "Hello World", speech_text))
+        return handler_input.response_builder.response
 
-@sb.request_handler(
-    can_handle_func=lambda handler_input:
-        is_intent_name("AMAZON.CancelIntent")(handler_input) or
-        is_intent_name("AMAZON.StopIntent")(handler_input))
-def cancel_and_stop_intent_handler(handler_input):
+class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "Goodbye!"
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
 
-    return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).response
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Goodbye!"
 
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+        return handler_input.response_builder.response
 
-@sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
-def fallback_handler(handler_input):
+class FallbackIntentHandler(AbstractRequestHandler):
     """AMAZON.FallbackIntent is only available in en-US locale.
     This handler will not be triggered except in that locale,
     so it is safe to deploy on any locale.
     """
-    # type: (HandlerInput) -> Response
-    speech = (
-        "The Hello World skill can't help you with that.  "
-        "You can say hello!!")
-    reprompt = "You can say hello!!"
-    handler_input.response_builder.speak(speech).ask(reprompt)
-    return handler_input.response_builder.response
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = (
+            "The Hello World skill can't help you with that.  "
+            "You can say hello!!")
+        reprompt = "You can say hello!!"
+        handler_input.response_builder.speak(speech_text).ask(reprompt)
+        return handler_input.response_builder.response
 
 
-@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
-def session_ended_request_handler(handler_input):
+class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
-    # type: (HandlerInput) -> Response
-    return handler_input.response_builder.response
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        return handler_input.response_builder.response
 
 
-@sb.exception_handler(can_handle_func=lambda i, e: True)
-def all_exception_handler(handler_input, exception):
+class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Catch all exception handler, log exception and
     respond with custom message.
     """
-    # type: (HandlerInput, Exception) -> Response
-    logger.error(exception, exc_info=True)
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
 
-    speech = "Sorry, there was some problem. Please try again!!"
-    handler_input.response_builder.speak(speech).ask(speech)
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
 
-    return handler_input.response_builder.response
+        speech = "Sorry, there was some problem. Please try again!!"
+        handler_input.response_builder.speak(speech).ask(speech)
+
+        return handler_input.response_builder.response
 
 
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(PhoneAuthIntentHandler())
+sb.add_request_handler(FacebookAuthIntentHandler())
+sb.add_request_handler(PhoneRequestCodeIntentHandler())
+sb.add_request_handler(PhoneAuthenticationIntentHandler())
+sb.add_request_handler(SetLocationIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
 
-    # if color_slot_key in handler_input.attributes_manager.session_attributes:
-    #     fav_color = handler_input.attributes_manager.session_attributes[
-    #         color_slot_key]
+sb.add_exception_handler(CatchAllExceptionHandler())
 
 lambda_handler = sb.lambda_handler()
 
